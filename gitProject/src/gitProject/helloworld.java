c0ccd076e9a1ba89a75469a0b0e96699b9408bed
Leonard <Leonard@LAPTOP-GP1NC36J>
2021-02-09 19:26:12 +0530

    first commit

---
+++ gitProject/src/gitProject/helloworld.java
@@ -0,0 +1,12 @@
+package gitProject;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+		int x;
+	}
+
+}
